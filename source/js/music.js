26c0434cd88e74ae49aee0e93896f82cffc6375e
xiaoma <17388758825@163.com>
2020-12-27 19:07:02 +0800

    音乐添加

---
+++ source/js/music.js
@@ -0,0 +1,17 @@
+let musicArray =new Array(
+"31654478",   //  Victory
+    "31654455",     //  Star Sky
+    "4010892",      //  Real
+    "4010720",      //  Oblivion
+)
+
+document.getElementById("music").innerHTML =
+    "<iframe " +
+        "frameborder='no' " +
+        "border='0' " +
+        "marginwidth='0' " +
+        "marginheight='0' " +
+        "width=330 " +
+        "height=86 " +
+        "src='//music.163.com/outchain/player?type=2&id="+musicArray[Math.floor(Math.random() * musicArray.length)]+"&userid=530079932&auto=1&height=66'>" +
+    "</iframe>";
