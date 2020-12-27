0a3360df2abc40abac960971811454c280b1146b
xiaoma <17388758825@163.com>
2020-12-27 20:51:29 +0800

    音乐不播放尝试修复

--- source/js/music.js
+++ source/js/music.js
@@ -1,4 +1,4 @@
-let musicArray =new Array(
+var musicArray =new Array(
 "31654478",   //  Victory
     "31654455",     //  Star Sky
     "4010892",      //  Real
@@ -13,5 +13,7 @@ document.getElementById("music").innerHTML =
         "marginheight='0' " +
         "width=330 " +
         "height=86 " +
-        "src='//music.163.com/outchain/player?type=2&id="+musicArray[Math.floor(Math.random() * musicArray.length)]+"&userid=530079932&auto=1&height=66'>" +
+        "src='//music.163.com/outchain/player?type=2&id=" +
+        musicArray[Math.floor(Math.random() * musicArray.length)] +
+        "&userid=530079932&auto=1&height=66'>" +
     "</iframe>";
