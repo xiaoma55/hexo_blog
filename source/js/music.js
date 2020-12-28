28c1f13dbaf4b628dffbbb556e800a4a8a20ad91
xiaoma <17388758825@163.com>
2020-12-28 14:02:46 +0800

    歌曲替换

--- source/js/music.js
+++ source/js/music.js
@@ -1,15 +1,25 @@
-const musicArray =new Array(
-// "31654478",   //  Victory
-//     "31654455",     //  Star Sky
-//     "4010892",      //  Real
-//     "4010720",      //  Oblivion
-    "1447361757",   //  沙悟净（翻自 河图）
+const musicArray =["31654478",   //  Victory
+    "31654455",     //  Star Sky
+    "27876900",     //  Here We Are Again
+    "1992463",      //  Awaken the Dawn (Solo)
+    "1614278",      //  Pastel Reflections
+    "28253999",     //  勿念他归
+    "4988618",      //  海の見える街
+    "760058",       //  Bloom of Youth
+    "527573",       //  想い出は遠くの日々
+    "3315209",      //  Send Me a Letter
+    "1236863",      //  Down by the Sally Gardens
+    "5293155",      //  햇살...바람...그리고 너
+    "28445798",     //  海の涙
+    "471415",       //  オルゴールの记忆
+    "28151036",     //  伝承
+    "757567"];
+
+const hanJingArray = ["1447361757", //  沙悟净（翻自 河图）
         "1293886117",   //  年少有为
         "1499297916",   //  男孩 (Live)
         "482207546",    //  拜无忧（Cover 萧忆情）
-        "420008229",    //  殿书
-
-)
+        "420008229"];
 
 const myMusic = document.getElementById("music");
 
