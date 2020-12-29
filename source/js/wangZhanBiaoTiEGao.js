049b3965553fa48b38dbcd9e7fc41afeeb0dfb3a
xiaoma <17388758825@163.com>
2020-12-29 11:40:29 +0800

    主题美化

---
+++ source/js/wangZhanBiaoTiEGao.js
@@ -0,0 +1,16 @@
+var OriginTitle = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+        $('[rel="icon"]').attr('href', "../img/icon/faviconRed.png");
+        document.title = '不要小宝贝啦ヽ(●-`Д´-)ノ！';
+        clearTimeout(titleTime);
+    }
+    else {
+        $('[rel="icon"]').attr('href', "../img/icon/favicon.png");
+        document.title = '你回来宝宝就开心ヾ(Ő∀Ő3)ノ！' + OriginTitle;
+        titleTime = setTimeout(function () {
+            document.title = OriginTitle;
+        }, 2000);
+    }
+});
