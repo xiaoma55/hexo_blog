1cfd48f0c2ee40e91358efa19790ecfd1d08f5ad
xiaoma <17388758825@163.com>
2020-12-31 16:14:54 +0800

    为刚哥添加音乐

--- source/js/wangZhanBeiJingYinYue.js
+++ source/js/wangZhanBeiJingYinYue.js
@@ -1,4 +1,5 @@
-const musicArray =["31654478",   //  Victory
+const musicArray =[
+    "31654478",   //  Victory
     "31654455",     //  Star Sky
     "27876900",     //  Here We Are Again
     "1992463",      //  Awaken the Dawn (Solo)
@@ -15,17 +16,22 @@ const musicArray =["31654478",   //  Victory
     "28151036",     //  伝承
     "757567"];
 
-const hanJingMusicArray = ["1447361757", //  沙悟净（翻自 河图）
+const hanJingMusicArray = [
+        "1447361757", //  沙悟净（翻自 河图）
         "1293886117",   //  年少有为
         "1499297916",   //  男孩 (Live)
         "482207546",    //  拜无忧（Cover 萧忆情）
         "420008229"];
 
+const wuTeGangMusicArray = [
+        "433107903"
+]
+
 const myMusic = document.getElementById("music");
 
 myMusic.innerHTML =
             "<source src='http://music.163.com/song/media/outer/url?id=" +
-             musicArray[Math.floor(Math.random() * musicArray.length)] +
+    wuTeGangMusicArray[Math.floor(Math.random() * wuTeGangMusicArray.length)] +
             "&userid=530079932' type='audio/mpeg' />";
 
 function play(){
